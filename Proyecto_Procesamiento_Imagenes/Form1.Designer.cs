4eef8d32c87b9e2b733c33275f73689e8f9a1d39
Chobbi <victor.perezml@uanl.edu.mx>
2022-01-19 17:03:06 -0600

    Primera prueba de camara funcionando

--- Proyecto_Procesamiento_Imagenes/Form1.Designer.cs
+++ Proyecto_Procesamiento_Imagenes/Form1.Designer.cs
@@ -29,8 +29,9 @@
         private void InitializeComponent()
         {
             this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.img_Camara = new System.Windows.Forms.PictureBox();
+            this.btn_Empezar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.img_Camara)).BeginInit();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -38,28 +39,39 @@
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(12, 12);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.Size = new System.Drawing.Size(285, 24);
             this.comboBox1.TabIndex = 0;
             // 
-            // pictureBox1
+            // img_Camara
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 59);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(398, 309);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.img_Camara.Location = new System.Drawing.Point(12, 59);
+            this.img_Camara.Name = "img_Camara";
+            this.img_Camara.Size = new System.Drawing.Size(675, 441);
+            this.img_Camara.TabIndex = 1;
+            this.img_Camara.TabStop = false;
+            // 
+            // btn_Empezar
+            // 
+            this.btn_Empezar.Location = new System.Drawing.Point(345, 12);
+            this.btn_Empezar.Name = "btn_Empezar";
+            this.btn_Empezar.Size = new System.Drawing.Size(134, 24);
+            this.btn_Empezar.TabIndex = 2;
+            this.btn_Empezar.Text = "Empezar";
+            this.btn_Empezar.UseVisualStyleBackColor = true;
+            this.btn_Empezar.Click += new System.EventHandler(this.btn_Empezar_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(422, 390);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(713, 527);
+            this.Controls.Add(this.btn_Empezar);
+            this.Controls.Add(this.img_Camara);
             this.Controls.Add(this.comboBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.img_Camara)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -67,6 +79,7 @@
         #endregion
 
         private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox img_Camara;
+        private System.Windows.Forms.Button btn_Empezar;
     }
 }
--- Proyecto_Procesamiento_Imagenes/Form1.cs
+++ Proyecto_Procesamiento_Imagenes/Form1.cs
@@ -7,11 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AForge.Video.DirectShow;
+using AForge.Video;
 
 namespace Proyecto_Procesamiento_Imagenes
 {
     public partial class Form1 : Form
     {
+        private bool isDevice = true;
+        private FilterInfoCollection misDispositivos;
+        private VideoCaptureDevice miWebCam;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,6 +25,49 @@ namespace Proyecto_Procesamiento_Imagenes
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            CargarDispositivos();
+        }
+
+        public void CargarDispositivos() 
+        {
+            misDispositivos = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (misDispositivos.Count > 0)
+            {
+                isDevice = true;
+                for (int i = 0; i < misDispositivos.Count; i++) 
+                    comboBox1.Items.Add(misDispositivos[i].Name.ToString());
+
+                comboBox1.Text = misDispositivos[0].ToString();
+            }
+            else 
+            {
+                isDevice = false;
+            }
+        }
+
+        public void CerrarWebCam() 
+        {
+            if (miWebCam != null && miWebCam.IsRunning) 
+            {
+                miWebCam.SignalToStop();
+                miWebCam = null;
+            }
+        }
+
+        private void btn_Empezar_Click(object sender, EventArgs e)
+        {
+            CerrarWebCam();
+            int i = comboBox1.SelectedIndex;
+            string NombreVideo = misDispositivos[i].MonikerString;
+            miWebCam = new VideoCaptureDevice(NombreVideo);
+            miWebCam.NewFrame += new NewFrameEventHandler(Capturando);
+            miWebCam.Start();
+        }
+
+        private void Capturando(object sender, NewFrameEventArgs eventArgs) 
+        {
+            Bitmap imagen = (Bitmap)eventArgs.Frame.Clone();
+            img_Camara.Image= imagen;
 
         }
     }
